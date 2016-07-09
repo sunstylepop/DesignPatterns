b5ff1c1db70d1f70004fe129709ddea0b70e8f21
sunstylepop <pop4855@gmail.com>
2016-07-09 17:56:13 +0800

    Add DecoratorPatten

---
+++ DecoratorPattern/FoodClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    //所有的食物都要繼承這個抽象類別
+    public abstract class FoodClass
+    {
+        protected int Price = 0;
+        protected string Descript = "";
+
+        public abstract int GetPrice();
+        public abstract string GetDescript();
+
+    }
+
+    /*********************主食系列*****************************/
+    public class Hamburger : FoodClass
+    {
+        public Hamburger()
+        {
+            this.Price = 35;
+            this.Descript = "漢堡";
+        }
+
+        public override int GetPrice()
+        {
+            return Price;
+        }
+
+        public override string GetDescript()
+        {
+            return Descript;
+        }
+    }
+
+    public class Rice : FoodClass
+    {
+        public Rice()
+        {
+            this.Price = 10;
+            this.Descript = "飯";
+        }
+
+        public override int GetPrice()
+        {
+            return Price;
+        }
+
+        public override string GetDescript()
+        {
+            return Descript;
+        }
+    }
+}
---
+++ DecoratorPattern/SideDish.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    public abstract class SideDish : FoodClass
+    {
+        //顧名思義就是多一個property用來裝飾food
+        FoodClass food;
+
+        public SideDish(FoodClass f)
+        {
+            this.food = f;
+        }
+
+        public override int GetPrice()
+        {
+            return this.Price + food.GetPrice();
+        }
+
+        public override string GetDescript()
+        {
+            return this.Descript + food.GetDescript();
+        }
+    }
+
+    /*********************附加食物系列*****************************/
+    public class Cheese : SideDish
+    {
+        public Cheese(FoodClass f) : base(f)
+        {
+            this.Price = 10;
+            this.Descript = "起司";
+        }
+    }
+
+    public class PorkChop : SideDish
+    {
+        public PorkChop(FoodClass f) : base(f)
+        {
+            this.Price = 50;
+            this.Descript = "豬排";
+        }
+    }
+
+    public class StewedPork  : SideDish
+    {
+        public StewedPork(FoodClass f) : base(f)
+        {
+            this.Price = 30;
+            this.Descript = "焢肉";
+        }
+    }
+}
